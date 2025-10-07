ca52ed8967470065d49065d986efb0716eefab4c
Taylor Plewe <tplewe@outlook.com>
2025-10-07 08:48:47 -0600

    add to build script; create 'bin' folder if doesn't exist

--- build.ps1
+++ build.ps1
@@ -1,3 +1,7 @@
+if (-not (Test-Path 'bin')) {
+    New-Item -ItemType Directory -Path "bin"
+}
+
 ml64 /Cp /Zd /Zi /Fo.\bin\asteroids86.obj src\main.s /link SDL3.lib /debug:full /entry:main /out:.\bin\asteroids86.exe
 
 if (Test-Path 'mllink$.lnk') {
