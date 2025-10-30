902cec66297357dccc109bc84c7fd827a64d8c69
Taylor Plewe <tplewe@outlook.com>
2025-10-30 08:49:16 -0600

    tweak some params

--- src/main.s
+++ src/main.s
@@ -109,8 +109,8 @@ main proc
 	lea rcx, [window_title]
 	mov edx, SCREEN_WIDTH
 	mov r8d, SCREEN_HEIGHT
-	xor r9d, r9d
-	; mov r9d, 1 ; fullscreen
+	; xor r9d, r9d
+	mov r9d, 1 ; fullscreen
 	call SDL_CreateWindow
 	mov qword ptr [window], rax
 
